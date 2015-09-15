287e79a24fccd69b324cc6124c8fbd3df86f9f88
Leah Klamkin & Austin Kincaid <student@epicodus.com>
2015-09-15 14:07:12 -0700

    Add button functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
+function Movie(filmTitle) {
+  this.filmTitle = filmTitle;
+  this.showtimes = [];
+}
+
 $(document).ready(function() {
   $("#add-movie").click(function() {
     $("#new-movie").show();
+
+    $("#add-showtime").click(function() {
+      var newShowtime = ('<div class="new-showtime">' +
+                          '<div class="form-group">' +
+                            '<label for="movie-time">Show Time</label>' +
+                            '<input type="time" id="movie-time" class="form-control" required="true">' +
+                          '</div>' +
+                        '</div>');
+      $(newShowtime).appendTo("#new-showtimes");
+    });
   });
 });
